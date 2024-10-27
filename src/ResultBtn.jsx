7c535ffdcd0d51b4c87ae6c082c2c0e69666a75a
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-27 18:52:26 +0530

    ratio issue solved

--- src/ResultBtn.jsx
+++ src/ResultBtn.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import * as htmlToImage from "html-to-image";
 
-export default function ResultBtn({ aspectRatio, className }) {
+export default function ResultBtn({ aspectRatio }) {
   const handleDownload = () => {
     const resultElement = document.getElementById("result");
 
     // Define the dimensions based on the selected aspect ratio
     let width, height;
     if (aspectRatio === "portrait") {
-      width = 1080;
-      height = 1350;
-    } else if (aspectRatio === "landscape") {
-      width = 1080;
-      height = 566;
-    } else {
-      // default to square
+      //default 1:1 ratio
       width = 1080;
       height = 1080;
+    } else if (aspectRatio === "landscape") {
+      width = 1080;
+      height = 608;
+    } else {
+      width = 1080;
+      height = 1350;
     }
 
-    // Create a temporary canvas
-    const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-
-    // Use html-to-image to create a PNG image
     htmlToImage
-      .toPng(resultElement, { canvas: canvas })
-      .then(function (dataUrl) {
+      .toPng(resultElement, {
+        width,
+        height,
+        canvasWidth: width,
+        canvasHeight: height,
+        pixelRatio: 1,
+        style: {
+          width: `${width}px`,
+          height: `${height}px`,
+          transform: "scale(1)",
+          transformOrigin: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      })
+      .then((dataUrl) => {
         const link = document.createElement("a");
         link.href = dataUrl;
         link.download = "result.png";
@@ -35,7 +44,7 @@ export default function ResultBtn({ aspectRatio, className }) {
         link.click();
         document.body.removeChild(link);
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.error("Error downloading image:", error);
       });
   };
@@ -55,7 +64,7 @@ export default function ResultBtn({ aspectRatio, className }) {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className={`mr-2 flex`}
+        className="mr-2 flex"
       >
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
         <polyline points="7 10 12 15 17 10"></polyline>
