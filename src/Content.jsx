4ac66f14c92d8a9a9b1176597875695a82bd8055
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-30 15:57:38 +0530

    v1.1.0

--- src/Content.jsx
+++ src/Content.jsx
@@ -11,11 +11,11 @@ const Content = ({ inputData, handleInputChange }) => {
   };
 
   return (
-    <section className="max-w-xl mx-auto p-4 space-y-6 sm:max-w-lg lg:max-w-xl">
+    <section className="max-w-xl mx-4 p-4 space-y-6 sm:max-w-lg lg:max-w-xl flex flex-col items-center">
       {/* Textarea for longer text input */}
 
       <textarea
-        className="textarea textarea-bordered w-full h-40 bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg lg:text-xl"
+        className="textarea textarea-bordered w-full max-w-md h-40 bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg lg:text-xl"
         name="thoughts"
         value={inputData.thoughts}
         onChange={handleTextareaChange}
@@ -27,7 +27,7 @@ const Content = ({ inputData, handleInputChange }) => {
 
       {/* First input field */}
       <input
-        className=" input input-bordered w-full bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg lg:text-xl"
+        className="input input-bordered w-full max-w-md bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg lg:text-xl"
         type="text"
         name="author"
         value={inputData.author}
@@ -37,7 +37,7 @@ const Content = ({ inputData, handleInputChange }) => {
 
       {/* Second input field */}
       <input
-        className=" input input-bordered w-full bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg  lg:text-xl"
+        className="input input-bordered w-full max-w-md bg-white text-black border-2 border-gray-300 rounded-lg shadow-sm focus:border-gray-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50 transition duration-300 ease-in-out text-lg lg:text-xl"
         type="text"
         name="insta_handle"
         value={inputData.insta_handle}
